30370ddc6700c582614947925ca3947f3c7a434d
Yaroslav Tsebro <yaroslav.cebro@gmail.com>
2023-09-14 18:22:31 +0300

    added service which server static

---
+++ backend/apps/static/src/static.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import Joi from 'joi';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      // envFilePath: './apps/articles/.env',
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number().required(),
+      }),
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'static'),
+    }),
+  ],
+})
+export class StaticModule {}
