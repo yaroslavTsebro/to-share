687afd3f1079798adb880a63bb39ccbbbc8195fc
Yaroslav Tsebro <yaroslav.cebro@gmail.com>
2023-09-19 18:15:45 +0300

    added cookie-parser

--- backend/apps/auth/src/main.ts
+++ backend/apps/auth/src/main.ts
@@ -3,11 +3,13 @@ import { AuthModule } from './auth.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
   const configService = app.get(ConfigService);
   app.useLogger(app.get(Logger));
+  app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   console.log(configService.get('PORT'));
   await app.listen(configService.get('PORT'));
