96e4db78756f3dd56b5878952e42807b53b16ce7
Yaroslav Tsebro <yaroslav.cebro@gmail.com>
2023-10-09 19:58:32 +0300

    added new endpoints

--- backend/apps/articles/src/controller/articles.controller.ts
+++ backend/apps/articles/src/controller/articles.controller.ts
@@ -3,7 +3,9 @@ import {
   Controller,
   Get,
   Inject,
+  ParseArrayPipe,
   Post,
+  Query,
   UploadedFiles,
   UseGuards,
   UseInterceptors,
@@ -13,10 +15,16 @@ import {
   ARTICLE_SERVICE,
   IArticlesService,
 } from '../service/articles-service.interface';
-import { ArticleDocument } from '../entity/schema/article.schema';
 import { FilesInterceptor } from '@nestjs/platform-express';
-import { CurrentUser, JwtAuthGuard, User } from '@app/common';
+import {
+  CurrentUser,
+  JwtAuthGuard,
+  SortValidationPipe,
+  User,
+} from '@app/common';
 import { CreateArticleDto } from '../entity/dto/create-article.dto';
+import { ResponseArticleDto } from '../entity/dto/response-article.dto';
+import { PaginationArticleDto } from '../entity/dto/pagination-article.dto';
 
 @Controller('articles')
 export class ArticlesController {
@@ -25,8 +33,28 @@ export class ArticlesController {
   ) {}
 
   @Get()
-  async getById(id: string): Promise<ArticleDocument> {
+  async getById(id: string): Promise<ResponseArticleDto> {
     return this.articlesService.getById(id);
+  }
+
+  @Get('/top')
+  async getTopByPagination(
+    @Query('page') page: number = 1,
+    @Query('perPage') perPage: number = 20,
+  ): Promise<PaginationArticleDto> {
+    return this.articlesService.getTopArticlesPagination(page, perPage);
+  }
+
+  @Get('')
+  async getByPagination(
+    @Query('page') page: number = 1,
+    @Query('perPage') perPage: number = 20,
+    @Query('query') query?: string,
+    @Query('tags', new ParseArrayPipe({ items: Number, separator: ',' }))
+    tags?: string[],
+    @Query('sortby', SortValidationPipe) sortQuery?: SortQuery[],
+  ): Promise<PaginationArticleDto> {
+    return this.articlesService.getTopArticlesPagination(page, perPage);
   }
 
   @Post()
@@ -36,7 +64,7 @@ export class ArticlesController {
     @CurrentUser() user: User,
     @UploadedFiles() files: Array<Express.Multer.File>,
     @Body() dto: CreateArticleDto,
-  ) {
-    this.articlesService.create(user, files, dto);
+  ): Promise<ResponseArticleDto> {
+    return this.articlesService.create(user, files, dto);
   }
 }
