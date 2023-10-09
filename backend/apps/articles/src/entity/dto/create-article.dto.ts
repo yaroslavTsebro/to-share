752b32de9ec5dc730f7baa387fa611f271bd747b
Yaroslav Tsebro <yaroslav.cebro@gmail.com>
2023-10-09 19:58:38 +0300

    minor changes

--- backend/apps/articles/src/entity/dto/create-article.dto.ts
+++ backend/apps/articles/src/entity/dto/create-article.dto.ts
@@ -1,11 +1,18 @@
-import { IsString, Length } from 'class-validator';
+import { IsArray, IsString, Length } from 'class-validator';
 
 export class CreateArticleDto {
   @Length(10, 60)
   @IsString()
   title: string;
 
+  @Length(10, 500)
+  @IsString()
+  description: string;
+
   @Length(10, 60)
   @IsString()
   content: string;
+
+  @IsArray()
+  tags: string[];
 }
