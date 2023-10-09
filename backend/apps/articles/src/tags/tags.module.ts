135727a51fab8a8a5953baea46a2a8577e405e7d
Yaroslav Tsebro <yaroslav.cebro@gmail.com>
2023-10-09 19:57:30 +0300

    minor changes

--- backend/apps/articles/src/tags/tags.module.ts
+++ backend/apps/articles/src/tags/tags.module.ts
@@ -1,4 +1,6 @@
+import { LoggerModule } from '@app/common';
 import { Module } from '@nestjs/common';
+import { TagRepository } from './repository/tag.repository';
 
-@Module({})
+@Module({ imports: [LoggerModule], exports: [TagRepository] })
 export class TagsModule {}
