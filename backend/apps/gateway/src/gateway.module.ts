5c3b27c262c829a8a5c33b637b497421aea79d64
Yaroslav Tsebro <yaroslav.cebro@gmail.com>
2023-10-09 19:56:52 +0300

    minor changes

--- backend/apps/gateway/src/gateway.module.ts
+++ backend/apps/gateway/src/gateway.module.ts
@@ -1,4 +1,5 @@
 import { IntrospectAndCompose } from '@apollo/gateway';
+import { LoggerModule } from '@app/common';
 import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
@@ -7,6 +8,7 @@ import * as Joi from 'joi';
 
 @Module({
   imports: [
+    LoggerModule,
     GraphQLModule.forRootAsync<ApolloGatewayDriverConfig>({
       driver: ApolloGatewayDriver,
       useFactory: (configService: ConfigService) => ({
--- backend/apps/gateway/src/main.ts
+++ backend/apps/gateway/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { GatewayModule } from './gateway.module';
 import { ConfigService } from '@nestjs/config';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(GatewayModule);
   const configService = app.get(ConfigService);
+  app.useLogger(app.get(Logger));
   console.log(configService.get('PORT'));
   await app.listen(configService.get('PORT'));
 }
