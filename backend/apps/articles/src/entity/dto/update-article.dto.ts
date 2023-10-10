002b908dd6621cd02d64bcb3959c20492daf5aca
Yaroslav Tsebro <yaroslav.cebro@gmail.com>
2023-10-10 17:08:37 +0300

    minor changes

--- backend/apps/articles/src/entity/dto/update-article.dto.ts
+++ backend/apps/articles/src/entity/dto/update-article.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateArticleDto } from './create-article.dto';
+import { IsArray } from 'class-validator';
 
-export class UpdateArticleDto extends PartialType(CreateArticleDto) {}
+export class UpdateArticleDto extends PartialType(CreateArticleDto) {
+  @IsArray()
+  deletedFileIds: number[];
+}
