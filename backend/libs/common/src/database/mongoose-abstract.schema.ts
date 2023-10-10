e338d722de7ffc560f2df3f663a3175b62be05e5
Yaroslav Tsebro <yaroslav.cebro@gmail.com>
2023-10-10 17:10:05 +0300

    minor changes

--- backend/libs/common/src/database/mongoose-abstract.schema.ts
+++ backend/libs/common/src/database/mongoose-abstract.schema.ts
@@ -1,8 +1,14 @@
 import { Prop, Schema } from '@nestjs/mongoose';
 import { SchemaTypes, Types } from 'mongoose';
 
-@Schema()
+@Schema({ timestamps: true })
 export class MongooseAbstractDocument {
   @Prop({ type: SchemaTypes.ObjectId })
   _id: Types.ObjectId;
+
+  @Prop({ default: Date.now })
+  createdAt: Date;
+
+  @Prop({ default: Date.now })
+  updatedAt: Date;
 }
