d6ce2b69444d29fcde581f3c63df35409b8e79b1
Yaroslav Tsebro <yaroslav.cebro@gmail.com>
2023-10-11 17:05:40 +0300

    added methods

--- backend/apps/comments/src/controller/comments.controller.ts
+++ backend/apps/comments/src/controller/comments.controller.ts
@@ -1,10 +1,62 @@
-import { Controller } from '@nestjs/common';
+import { COMMENTS_SERVICE, CurrentUser, JwtAuthGuard, User } from '@app/common';
+import { ICommentsService } from '../service/comments-service.interface';
+import {
+  Body,
+  Controller,
+  Get,
+  Inject,
+  Param,
+  Post,
+  Put,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
+import { UpdateCommentDto } from '../entity/dto/update-comment.dto';
+import { CreateCommentDto } from '../entity/dto/create-comment.dto';
+import { Comment } from '../entity/comment.entity';
 
-@Controller()
+@Controller('comments')
 export class CommentsController {
-  // constructor(private readonly commentsService: CommentsService) {}
-  // @Get()
-  // getHello(): string {
-  //   return this.commentsService.getHello();
-  // }
+  constructor(
+    @Inject(COMMENTS_SERVICE)
+    private readonly commentsService: ICommentsService,
+  ) {}
+
+  @Get(':articleId')
+  async getComments(
+    @Param('articleId') id: string,
+    @Query('page') page: number = 1,
+    @Query('perPage') perPage: number = 20,
+  ): Promise<Comment[]> {
+    return this.commentsService.getComments(id, page, perPage);
+  }
+
+  @Get('/replies/:id')
+  async getReplies(
+    @Param('id') id: number,
+    @Query('page') page: number = 1,
+    @Query('perPage') perPage: number = 20,
+  ): Promise<Comment[]> {
+    return this.commentsService.getReplies(id, page, perPage);
+  }
+
+  @Put(':id')
+  @UseGuards(JwtAuthGuard)
+  async updateById(
+    @Param('id') id: string,
+    @CurrentUser() user: User,
+    @Body() dto: UpdateCommentDto,
+  ): Promise<string> {
+    return this.commentsService.updateById(id, user, dto);
+  }
+
+  @Post(':articleId')
+  @UseGuards(JwtAuthGuard)
+  async create(
+    @Param('articleId') id: string,
+    @CurrentUser() user: User,
+    @Body() dto: CreateCommentDto,
+  ): Promise<string> {
+    return this.commentsService.create(id, user, dto);
+  }
 }
