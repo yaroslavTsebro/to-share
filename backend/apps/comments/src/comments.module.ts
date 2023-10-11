dec384399091be651f9d17a049610db585dd376e
Yaroslav Tsebro <yaroslav.cebro@gmail.com>
2023-10-11 17:04:55 +0300

    added endpoints

--- backend/apps/comments/src/comments.module.ts
+++ backend/apps/comments/src/comments.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { CommentsService } from './service/comments.service';
+import { COMMENTS_SERVICE } from '@app/common';
+import { CommentRepository } from './repository/comment.repository';
 
 @Module({
   imports: [],
   // controllers: [CommentsController],
-  providers: [CommentsService],
+
+  providers: [
+    CommentRepository,
+    {
+      useClass: CommentsService,
+      provide: COMMENTS_SERVICE,
+    },
+  ],
 })
 export class CommentsModule {}
