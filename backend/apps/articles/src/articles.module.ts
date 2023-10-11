cb723369e1df53df512af2ef97fc8acd3afa32d1
Yaroslav Tsebro <yaroslav.cebro@gmail.com>
2023-10-11 17:05:29 +0300

    added rabitmq clients

--- backend/apps/articles/src/articles.module.ts
+++ backend/apps/articles/src/articles.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ArticlesService } from './service/articles.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as Joi from 'joi';
 import { ArticlesController } from './controller/articles.controller';
 import { ARTICLE_SERVICE } from './service/articles-service.interface';
 import { TagsModule } from './tags/tags.module';
-import { LoggerModule } from '@app/common';
+import { AUTH_SERVICE, LoggerModule, STATIC_SERVICE } from '@app/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
   imports: [
@@ -18,6 +19,30 @@ import { LoggerModule } from '@app/common';
       }),
     }),
     TagsModule,
+    ClientsModule.registerAsync([
+      {
+        name: AUTH_SERVICE,
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.RMQ,
+          options: {
+            urls: [configService.getOrThrow<string>('RABBITMQ_URI')],
+            queue: 'auth',
+          },
+        }),
+        inject: [ConfigService],
+      },
+      {
+        name: STATIC_SERVICE,
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.RMQ,
+          options: {
+            urls: [configService.getOrThrow<string>('RABBITMQ_URI')],
+            queue: 'static',
+          },
+        }),
+        inject: [ConfigService],
+      },
+    ]),
   ],
   controllers: [ArticlesController],
   providers: [
